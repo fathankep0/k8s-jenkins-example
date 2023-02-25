2b239c2818aa77a78ed7eb7c503d4feea29c40ad
fathankep0 <86276237+fathankep0@users.noreply.github.com>
2023-02-25 21:05:29 +0700

    Update shared-library.groovy

--- .mvn/wrapper/vars/shared-library.groovy
+++ .mvn/wrapper/vars/shared-library.groovy
@@ -1,3 +1,26 @@
-@library('jenkins@master)_
+@library("shared-library@master")_ 
+def call {
+}
+pipeline {
+  agent any
+  environment {
+    def githubRepo = "https://github.com/fathankep0/k8s-jenkins-example"
+    def githubCredId ="github"
+  }
+  stages {
+    stage ('preperation shared-library') {
+      steps {
+        script {
+          sh """
+          printenv | sort
+          echo githubUrl = ${"env.githubUrl"}
+          echo githubRepo = ${"env.githubRepo"}
+          echo github= ${"env.github"}
+          """
+        }
+      }
+    }
+  }
+}
 
                         
