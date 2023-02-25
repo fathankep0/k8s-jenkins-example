204f421f4447126152ae6e4eb337c77bde62590f
fathankep0 <86276237+fathankep0@users.noreply.github.com>
2023-02-25 20:26:57 +0700

    Update shared-library.groovy

--- .mvn/wrapper/vars/shared-library.groovy
+++ .mvn/wrapper/vars/shared-library.groovy
@@ -1,27 +1,12 @@
-@library("shared-library@master")_
-
-pipeline {
-    agent any
-    environment {
-        def githubUrl = "api.github.com"
-        def githubRepo = "https://github.com/fathankep0/k8s-jenkins-example.git"
-        def credentialsId = "github"
-        def ansibleCred = ""
-        stages {
-            stage ('parameters') {
-                steps {
-                    script {
-                        sh """
-                        printenv
-                        echo githubUrl = "${env.githubUrl}"
-                        echo githubRepo = "${env.githubRepo}"
-                        echo credentialsId = "${env.credentialsId}"
-                        echo ansibleCred = "${env.ansibleCred}"
-                        
-                        """
-                    }
-                }
-            }
+def (githubUrl) {
+    def (githubRepo) {
+        def (credentialsId) {
+            echo.("githubUrl") 
+            echo.("githubRepo")
+            echo.("credentialsId")
         }
     }
 }
+
+
+                        
